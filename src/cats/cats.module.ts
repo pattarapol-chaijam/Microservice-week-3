b9d70778ffb368679ff10f7e3568862a2a96bdee
Pattarapol Chaijam <64160249@go.buu.ac.th>
2024-07-28 18:37:04 +0700

    add cat modules

--- src/cats/cats.module.ts
+++ src/cats/cats.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { CatsController } from './cats.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Cat, CatSchema } from './schemas/cat.schemas';
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Cat.name, schema: CatSchema }])],
   controllers: [CatsController],
   providers: [CatsService],
 })
